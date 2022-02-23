bf8fdc46cc0b9194f3ba9b82f45b1e703980b174
Konstaphy <Konstaphy@gmail.com>
2022-02-24 00:52:57 +0500

    enhancement: header

--- client/src/app/header/header.tsx
+++ client/src/app/header/header.tsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./header.scss";
 
 const Header: React.FC = () => {
-  return <header className="header">Header</header>;
+  return (
+    <header className="header">
+      <div className="header__logo">Konstaphy</div>
+      <ul className="header__links">
+        <li>About me</li>
+        <li>Stack</li>
+        <li>Purposes</li>
+        <li>Demos</li>
+      </ul>
+    </header>
+  );
 };
 
 export default Header;
