eb492d84235328f3295f4ce84b9422f44da91eb6
Konstantine <Konstaphy@gmail.com>
2022-03-03 23:57:17 +0500

    enhancement: template

--- client/src/app/content/content.tsx
+++ client/src/app/content/content.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./content.scss";
 
 const Content: React.FC = () => {
-  return <main className="content">Content</main>;
+  return (
+    <main className="content">
+      <div className="content-wrapper">Content</div>
+    </main>
+  );
 };
 
 export default Content;
